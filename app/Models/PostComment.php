b9f665e6e5bc81800396d7473f001ec554ca8fab
7487938887 <anton.kulik.dev@gmail.com>
2021-09-26 10:26:54 +0300

    simplify models

--- app/Models/PostComment.php
+++ app/Models/PostComment.php
@@ -2,27 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class PostComment extends Model
 {
-    use HasFactory;
-
-    /**
-     * Get the table associated with the model.
-     *
-     * @return string
-     */
-    public function getTable()
-    {
-        if (! isset($this->table)) {
-            return str_replace(
-                '\\', '', Str::snake(Str::plural(class_basename($this)))
-            );
-        }
-        return $this->table;
-    }
-
+    //
 }
